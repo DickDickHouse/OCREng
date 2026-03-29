235e1e39717e77cea943eedac007ad529686f330
DickDickHouse <dickcheungsf@gmail.com>
2026-03-29 16:58:06 +0800

    Add game configuration constants
    Added configuration constants for board size, colors, launch rules, and dice images.

--- config.js
+++ config.js
@@ -1,1 +1,24 @@
+// config.js
+export const BOARD_ROWS = 15;
+export const BOARD_COLS = 15;
+export const PATH_LENGTH = 222; // 根據您的路徑長度調整
 
+export const COLORS = ["red", "blue", "green", "yellow"];
+export const COLOR_NAMES = {
+    red: "紅棋",
+    blue: "藍棋",
+    green: "綠棋",
+    yellow: "黃棋",
+};
+
+export const LAUNCH_RULE_DEFAULT = "6"; // "6", "even", "odd"
+
+export const DICE_IMAGES = {
+    blank: "images/dice-blank.svg",
+    1: "images/dice-1.svg",
+    2: "images/dice-2.svg",
+    3: "images/dice-3.svg",
+    4: "images/dice-4.svg",
+    5: "images/dice-5.svg",
+    6: "images/dice-6.svg",
+};
