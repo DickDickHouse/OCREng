a48308308b46a91d683e91bdd4b69bc791119bf9
DickDickHouse <dickcheungsf@gmail.com>
2026-03-30 18:13:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // main.js
-import Game from './Game.js';
+import Game from "Game.js";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', ()=> {
     console.log('[OOP] 開始初始化...');
-    const game = new Game();
+    const game=new Game();
     console.log('[OOP] 初始化完成！');
 });
