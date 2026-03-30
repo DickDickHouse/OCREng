b10c3b2621caa94593c84606f759d2d839cdd03a
DickDickHouse <dickcheungsf@gmail.com>
2026-03-30 17:32:22 +0800

    Initialize game on DOM content loaded

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+// main.js
+import Game from './Game.js';
 
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('[OOP] 開始初始化...');
+    const game = new Game();
+    console.log('[OOP] 初始化完成！');
+});
